4b1fb0232a17f9b22952dee5fe03c6667426faa2
ArmanHossain04 <armanhossain0477@gmail.com>
2025-08-03 13:46:40 +0600

    learn: templete string , arrow funtion and spread operatior

---
+++ templete.js
@@ -0,0 +1,21 @@
+// templete sting 
+const fName = "arman "
+const lName = "hossain"
+// console.log(`i am ${fName+lName}`);
+
+
+
+// arrow funtion 
+const sum = (a, b) => {
+    const summition = a+b;
+    const div = summition /2;
+    return div
+}
+// console.log(sum(1,2));
+
+
+
+// sprad operatior 
+const numbers = [1,2,3,4,5,6];
+const newNumbers = [...numbers, 10, 100]
+console.log(newNumbers);
